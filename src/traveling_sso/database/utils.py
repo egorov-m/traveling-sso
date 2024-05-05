06d04a8577755a52b07b2145b3dcc5a224c8c505
Egorov Mikhail <mikhailegorov_dev@outlook.com>
2024-05-06 01:39:24 +0500

    Reconfiguration database connection

--- src/traveling_sso/database/utils.py
+++ src/traveling_sso/database/utils.py
@@ -1,4 +1,37 @@
 from datetime import datetime, timezone
+from enum import IntEnum
+from functools import wraps
+
+
+class CommitMode(IntEnum):
+    """
+    Commit modes for the managed db methods
+    """
+
+    NONE = 0
+    FLUSH = 1
+    COMMIT = 2
+    ROLLBACK = 3
+
+
+def menage_db_commit_method(auto_commit: CommitMode = CommitMode.FLUSH):
+    def decorator(f):
+        @wraps(f)
+        async def wrapped_f(self, *args, **kwargs):
+            result = await f(self, *args, **kwargs)
+            match auto_commit:
+                case CommitMode.FLUSH:
+                    await self.session.flush()
+                case CommitMode.COMMIT:
+                    await self.session.commit()
+                case CommitMode.ROLLBACK:
+                    await self.session.rollback()
+
+            return result
+
+        return wrapped_f
+
+    return decorator
 
 
 def utcnow() -> datetime:
