30982cf1c3a66a20f12bc9dd7912ff7ee8683633
Egorov Mikhail <mikhailegorov_dev@outlook.com>
2024-05-17 02:41:38 +0500

    fix: swagger ui url

--- src/traveling_sso/main.py
+++ src/traveling_sso/main.py
@@ -7,6 +7,7 @@ from asgi_correlation_id.middleware import is_valid_uuid4
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationException, RequestValidationError
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from starlette import status
@@ -26,10 +27,28 @@ app = FastAPI(
     description=settings.PROJECT_DESCRIPTION,
     version=settings.PROJECT_VERSION,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    docs_url=f"{settings.API_V1_STR}/docs",
-    redoc_url=f"{settings.API_V1_STR}/redoc",
     servers=settings.API_SERVERS
 )
+
+
+@app.get(f"{settings.API_V1_STR}/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css",
+    )
+
+
+@app.get(f"{settings.API_V1_STR}/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
+    )
+
 
 app.include_router(app_router, prefix=settings.API_V1_STR)
 
